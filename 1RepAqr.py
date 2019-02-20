25ed72607cdaac57e1c4a3f33499f05d3136afd8
”Boscoo” <gugu.bosco13@gmail.com>
2019-02-20 11:40:28 -0300

    Meu primeiro aui

---
+++ 1RepAqr.py
@@ -0,0 +1,10 @@
+import os
+from os import system as sys
+
+for i in range(10):
+    print("Este é meu primeiro programa em um repositorio no Github")
+    sys("sleep 0.5")
+    sys("clear")
+    sys("sleep 0.2")
+    if i == 9:
+        print("Acabou")
