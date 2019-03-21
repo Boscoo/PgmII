57230248e2912160bb3edc7faa302f387301b3a9
Boscoo <gugu.bosco13@gmail.com>
2019-03-21 09:04:36 -0300

    Recebendo

--- server/server.py
+++ server/server.py
@@ -1,11 +1,26 @@
 from pessoa import Pessoa
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
-lista = [Pessoa("Fulano", "Rua 4", "3333-3333"),
-         Pessoa("Detal", "Rua cinza", "3333-2244")
-        ]
+lista = []
 app = Flask(__name__)
 @app.route("/")
 def start():
-    return render_template("listar_pessoa.html", pessoa=lista)
+        return render_template("inicio.html")
+
+@app.route("/listar_pessoa")
+def listar():
+        return render_template("listar_pessoa.html", pessoa=lista)
+
+@app.route("/form_inserir_pessoa")
+def insert():
+        return render_template("form_inserir_pessoa.html")
+
+@app.route("/adicionar_pessoa")
+def add():
+        nome = request.args.get("nome")
+        endereco = request.args.get("endereco")
+        telefone = request.args.get("telefone")
+        identificacao = Pessoa(nome, endereco, telefone)
+        lista.append(identificacao)
+        return render_template("exibir_mensagem.html")
 app.run()
