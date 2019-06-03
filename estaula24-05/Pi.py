cf9b8ba29884e32110e27a119424720ece5f4cbb
Boscoo <gugu.bosco13@gmail.com>
2019-06-03 14:29:18 -0300

    aula03-06II

--- estaula24-05/Pi.py
+++ estaula24-05/Pi.py
@@ -13,6 +13,10 @@ class Aluno(Pessoa):
         self.matricula = matricula
         self.curso     = curso
         self.turma     = turma
+    
+    def __str__(self):
+        return "Nome: " + self.nome + ", Idade: " + str(self.idade) + ", Telefone: " + str(self.telefone) + ", Matricula: " + str(self.matricula) +\
+        ", Curso: " + self.curso + ", Turma: " + self.turma
 
 class Professor(Pessoa):
     def __init__(self, nome, idade, telefone, codProf, areas):
@@ -30,9 +34,16 @@ class PI:
 class Periodicos:
     def __init__(self, ISSN, editora):
         self.ISSN    = ISSN
-        self.editora = editora]
+        self.editora = editora
 
 class Eventos:
     def __init__(self, data, local):
         self.data  = data
         self.local = local
+
+if __name__ == "__main__":
+
+    pessoa1 = Pessoa("David Hume", 41, "9654-5496")
+    aluno1 = Aluno("Hume David", 14, "5496-9654", "1123", "Informatica", "405")
+    print(pessoa1)
+    print(aluno1)
